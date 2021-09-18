44b048549d89cff04597b34767390621b24b0ce8
Thiago Figueiró <thiagocsf@gmail.com>
2021-09-18 11:04:01 +1000

    Make LOG_LEVEL case insensitive

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from loguru import logger
 from app.main import app
 
 
-LOG_LEVEL = logging.getLevelName(os.environ.get("LOG_LEVEL", "INFO"))
+LOG_LEVEL = logging.getLevelName(os.environ.get("LOG_LEVEL", "INFO").upper())
 JSON_LOGS = True if os.environ.get("JSON_LOGS", "0") == "1" else False
 WORKERS = int(os.environ.get("GUNICORN_WORKERS", "5"))
 
